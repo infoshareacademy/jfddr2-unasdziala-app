d3944b8ed9fe2698c0bdf601d63f5b8f8be14262
Liliia Saleck <lilisaleck@gmail.com>
2021-02-10 15:08:28 +0100

    Work in progress. useState training

--- skillshop/src/App.js
+++ skillshop/src/App.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Service from "./components/Service";
 import ServicesList from "./components/ServicesList";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  fetch("./services.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+  const KlikButton = () => {
+    let AktualnyStan = counter;
+    AktualnyStan++;
+    setCounter(AktualnyStan);
+  };
   return (
     <>
       <Navbar />
-      <ServicesList />
-      <Service />
+      <ServicesList>
+        <Service />
+      </ServicesList>
+      <p>{counter}</p>
+      <button onClick={KlikButton}>Kliknij mnie</button>
     </>
   );
 }
