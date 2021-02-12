9cf46a847ebcb905b67c6af8efc45daac71891b4
Janusz Różański <januszjanero@gmail.com>
2021-02-12 14:31:38 +0100

    Add Route (link and switch)

--- skillshop/src/App.js
+++ skillshop/src/App.js
@@ -1,4 +1,5 @@
 // import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Service from "./components/Service";
 import ServicesList from "./components/ServicesList";
@@ -19,14 +20,28 @@ function App() {
   //   setCounter(AktualnyStan);
   // };
   return (
-    <div>
-      <Navbar></Navbar>
-      <ServicesList>
-        <Service />
-      </ServicesList>
-      {/* <p>{counter}</p> */}
-      {/* <button onClick={KlikButton}>Kliknij mnie</button> */}
-    </div>
+    <Router>
+      <Navbar>
+        <Link to="/">Home</Link>
+        <Link to="/uslugi">About</Link>
+        <Link to="/users">Users</Link>
+      </Navbar>
+
+      <Switch>
+        <Route path="/uslugi">
+          <ServicesList />
+        </Route>
+        <Route path="/users">{/* <Users /> */}</Route>
+        <Route path="/">{/* <Home /> */}</Route>
+      </Switch>
+      <div>
+        {/* <ServicesList>
+          <Service />
+        </ServicesList> */}
+        {/* <p>{counter}</p> */}
+        {/* <button onClick={KlikButton}>Kliknij mnie</button> */}
+      </div>
+    </Router>
   );
 }
 
