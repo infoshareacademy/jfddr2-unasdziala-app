24ee40acffe2e7b78e87111e4332faba31415c6a
mitroc <kontomtr@gmail.com>
2021-02-25 01:55:12 +0100

    refactor: simplify structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import MainContainer from "./components/MainContainer";
+import { useEffect, useState } from "react";
+import firebase from "./firebase/config";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./styles/MainContainer.css";
 
 function App() {
-  return <MainContainer />;
+  const [dummyVariable, setDummyVariable] = useState([]);
+  const [serviceList, setServiceList] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("services")
+      .get()
+      .then((snapshot) => {
+        const services = snapshot.docs.map((service) => {
+          return {
+            ...service.data(),
+            id: service.id,
+          };
+        });
+        setServiceList(services);
+      });
+  }, [dummyVariable]);
+
+  return (
+    <div className="main-container">
+      <Navbar services={serviceList} />
+      <Main
+        serviceList={serviceList}
+        setServiceList={setServiceList}
+        setDummyVariable={setDummyVariable}
+      />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
