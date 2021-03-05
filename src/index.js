088340eb354ae5704fac0abcfba11732dc81475f
Maciej Paluszyński <macpalus@gmail.com>
2021-03-05 09:01:09 +0100

    gh-pages actualization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { HashRouter } from 'react-router-dom'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter basename={process.env.PUBLIC_URL}>
+       <App />
+     </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
